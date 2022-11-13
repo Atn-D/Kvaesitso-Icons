cc4bf0b2eb202f242a9e9904cb7edfcdcb66da84
daywalk3r666 <markushelm3@gmail.com>
2022-11-13 10:49:38 +0100

    Update repo name

--- send_notifications.py
+++ send_notifications.py
@@ -56,7 +56,7 @@ commits = list(repository.iter_commits(commits_range))
 
 overview_link = f'https://github.com/{github_repository}/compare/{commits_range}'
 overview_link_tag = f'''<a href="{overview_link}">{len(commits)} new commit{'s' if len(commits) > 1 else ''}</a>'''
-message = f'''<b>🔨 {overview_link_tag} to <code>lawnicons:{github_ref}</code>:</b>\n'''
+message = f'''<b>🔨 {overview_link_tag} to <code>Kvaesitso-Icons:{github_ref}</code>:</b>\n'''
 
 for commit in reversed(commits):
     commit_message = commit.message.split('\n')[0]
