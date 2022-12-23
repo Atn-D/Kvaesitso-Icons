a75143d8cf43b79a7e51774d000b55e465e61db1
daywalk3r666 <markushelm3@gmail.com>
2022-12-23 18:24:49 +0100

    Bump Compose Material 3 to 1.1.0-alpha03

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,7 +103,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.compose.animation:animation")
     implementation("androidx.compose.material:material")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha02")
+    implementation("androidx.compose.material3:material3:1.1.0-alpha03")
     implementation("androidx.core:core-splashscreen:1.0.0-beta02")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
