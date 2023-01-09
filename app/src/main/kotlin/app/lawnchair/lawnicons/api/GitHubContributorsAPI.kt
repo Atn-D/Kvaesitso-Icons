dc53c962bd88412a208a148e093b64af3f8420d9
daywalk3r666 <markushelm3@gmail.com>
2023-01-09 16:58:20 +0100

    Test

--- app/src/main/kotlin/app/lawnchair/lawnicons/api/GitHubContributorsAPI.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/api/GitHubContributorsAPI.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 
 interface GitHubContributorsAPI {
 
-    @GET("repos/LawnchairLauncher/lawnicons/contributors")
+    @GET("repos/daywalk3r666/kvaesitso-icons/contributors")
     suspend fun getContributors(): List<GitHubContributor>
 }
